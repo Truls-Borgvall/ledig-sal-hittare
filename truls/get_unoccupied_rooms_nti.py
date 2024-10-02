b044caf7fb6bdc031d4b4e88efff1131c65bcd7e
Truls-Borgvall <truls.borgvall@elev.ga.ntig.se>
2024-10-02 10:04:53 +0200

    worked don get_oncuppied_rooms

--- truls/get_unoccupied_rooms_nti.py
+++ truls/get_unoccupied_rooms_nti.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 import json
 
 def get_unoccupied_rooms():
+    unoccupied_rooms = []
     session = requests.Session()
     
     headers = {
@@ -61,31 +62,62 @@ def get_unoccupied_rooms():
     rooms =  data["data"]["rooms"]
     for room in rooms:
         json_data = None
-    response = session.post('https://web.skola24.se/api/get/timetable/render/key', headers=headers, json=json_data)
-    data =response.json()
-    key = data['data']['key']
+        response = session.post('https://web.skola24.se/api/get/timetable/render/key', headers=headers, json=json_data)
+        data =response.json()
+        key = data['data']['key']
 
-    json_data = {
-        'renderKey': key,#'8Cwray_A1HVYgPUAhD7_SM5ZXClAiEAmn8WBBqYXq1QX-NebheYJzqFcBrSssuB9ZIvnPfV-xFpHsHnaJhLzMlMUEenq5vLgWFjatgmGcAkP5GvOlO635mSG4Z6iA0kG'
-        'host': 'it-gymnasiet.skola24.se',
-        'unitGuid': nti_johanneberg_guid,
-        'schoolYear': year_guid,
-        'startDate': None,
-        'endDate': None,
-        'scheduleDay': 0,
-        'blackAndWhite': False,
-        'width': 1206,
-        'height': 550,
-        'selectionType': 3,
-        'selection': room["eid"],
-        'showHeader': False,
-        'periodText': '',
-        'week': 37,
-        'year': 2024,
-        'privateFreeTextMode': None,
-        'privateSelectionMode': False,
-        'customerKey': '',
-    }
-    response = session.post('https://web.skola24.se/api/render/timetable', headers=headers, json=json_data)
-    data = response.json()
-    schedule_data = data["data"]["lessonInfo"]
+        json_data = {
+            'renderKey': key,#'8Cwray_A1HVYgPUAhD7_SM5ZXClAiEAmn8WBBqYXq1QX-NebheYJzqFcBrSssuB9ZIvnPfV-xFpHsHnaJhLzMlMUEenq5vLgWFjatgmGcAkP5GvOlO635mSG4Z6iA0kG'
+            'host': 'it-gymnasiet.skola24.se',
+            'unitGuid': nti_johanneberg_guid,
+            'schoolYear': year_guid,
+            'startDate': None,
+            'endDate': None,
+            'scheduleDay': 0,
+            'blackAndWhite': False,
+            'width': 1206,
+            'height': 550,
+            'selectionType': 3,
+            'selection': room["eid"],
+            'showHeader': False,
+            'periodText': '',
+            'week': 37,
+            'year': 2024,
+            'privateFreeTextMode': None,
+            'privateSelectionMode': False,
+            'customerKey': '',
+        }
+        response = session.post('https://web.skola24.se/api/render/timetable', headers=headers, json=json_data)
+        data = response.json()
+        schedule_data = data["data"]["lessonInfo"]
+        
+        date_time = datetime.now()  # Default to current time
+
+        current_day = date_time.weekday() + 1  # Get day of the week (1=Monday, 7=Sunday)
+        current_time = date_time.time()        # Get the current time
+
+        occupied = False
+        unoccupied_until = None
+
+        for entry in schedule_data:
+            if entry['dayOfWeekNumber'] == current_day:
+                
+                start_time = datetime.strptime(entry['timeStart'], '%H:%M:%S').time()
+                end_time = datetime.strptime(entry['timeEnd'], '%H:%M:%S').time()
+
+                if start_time <= current_time <= end_time:
+                    occupied = True
+                    break
+                elif current_time < start_time and (unoccupied_until is None or start_time < unoccupied_until):
+                    unoccupied_until = start_time
+
+
+        if unoccupied_until:
+            time_until_next_occupation = datetime.combine(date_time.date(), unoccupied_until) - date_time
+            unoccupied_rooms.append({"name": room["name"], "unoccupied_until": unoccupied_until.strftime('%H:%M')})
+            print(schedule_data)
+        else:
+            unoccupied_rooms.append({"name": room["name"], "unoccupied_until": "24:00"})
+        return unoccupied_rooms
+
+print(get_unoccupied_rooms())
